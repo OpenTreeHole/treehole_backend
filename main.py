7eae9f921e11ad8d7834b894a4143bff2c815a2d
顶碗人 <jsclndnz@outlook.com>
2022-04-13 20:00:49 +0800

    Revert "log headers"
    This reverts commit 58fc67494b418c02ee1a69cb6393e7da6b236ac4.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 
 app = FastAPI()  # app 实例化位于所有导入之前
 from bbs import division, floor, hole
@@ -9,9 +9,5 @@ app.include_router(hole.router)
 
 
 @app.get('/')
-async def home(request: Request):
-    for (k, v) in request.headers.items():
-        k: str
-        if k.lower().startswith('x'):
-            print(k, v)
+async def home():
     return {'message': 'hello world'}
