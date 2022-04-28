fe53eaf7f7a980d1232c962e1701b83b395cf45f
顶碗人 <jsclndnz@outlook.com>
2022-04-28 18:15:54 +0800

    feat: profiler in dev environment

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import asyncio
-import multiprocessing
-import time
-
-import uvicorn
-from aerich import Command
+from pyinstrument import Profiler
 from starlette.requests import Request
 
 from bbs import division, floor, hole
-from config import TORTOISE_ORM, config
+from config import config
 from utils.common import get_ip
 from utils.patch import MyFastAPI
 
@@ -27,27 +22,12 @@ async def home(request: Request):
 
 if config.debug:
     @app.middleware('http')
-    async def add_process_time_header(request: Request, call_next):
-        start_time = time.time()
+    async def profile(request: Request, call_next):
+        profiler = Profiler()
+        profiler.start()
         response = await call_next(request)
-        process_time = time.time() - start_time
-        response.headers['X-Process-Time'] = f'{process_time * 1000:.1f} ms'
+        profiler.stop()
+        with open('data/profile.html', 'w', encoding='utf-8') as f:
+            f.write(profiler.output_html())
+
         return response
-
-
-async def migrate():
-    command = Command(tortoise_config=TORTOISE_ORM)
-    await command.init()
-    await command.migrate()
-    await command.upgrade()
-
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(migrate())
-    uvicorn.run(
-        app='main:app', host='0.0.0.0', port=8000,
-        workers=1 if config.debug else multiprocessing.cpu_count(),
-        reload=config.debug,
-        log_level='info'
-    )
---
+++ start.py
@@ -0,0 +1,25 @@
+import asyncio
+import multiprocessing
+
+import uvicorn
+from aerich import Command
+
+from config import TORTOISE_ORM, config
+
+
+async def migrate():
+    command = Command(tortoise_config=TORTOISE_ORM)
+    await command.init()
+    await command.migrate()
+    await command.upgrade()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(migrate())
+    uvicorn.run(
+        app='main:app', host='0.0.0.0', port=8000,
+        workers=1 if config.debug else multiprocessing.cpu_count(),
+        reload=config.debug,
+        log_level='info'
+    )
